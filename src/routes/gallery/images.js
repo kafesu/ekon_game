598ac0d70486c8febd3e70f360c85f7d3025d8c5
Anesu Kafesu <anesukafesu@gmail.com>
2022-11-28 16:02:51 +0200

    Converted  HEIC images to JPEG and fixed the gallery

--- src/routes/gallery/images.js
+++ src/routes/gallery/images.js
@@ -6,13 +6,13 @@ export const images = [
 	'https://drive.google.com/uc?export=view&id=1pctLxCR_OKOZ6stZgkErNkFqBv0i7RdS',
 	'https://drive.google.com/uc?export=view&id=1iaD_psrkHqxpJvfpPqKh6je1_-PuDpaB',
 	'https://drive.google.com/uc?export=view&id=1l9Z7pEkPpAsvfcgoHUgEgGs5H1GyYiIP',
-	'https://drive.google.com/uc?export=view&id=1i2DDJjocvdjymMJMg2j9R6re0l0CntqL',
-	'https://drive.google.com/uc?export=view&id=1S7ICYm5tpEfyXJ3z6F6fnFvVnacqtIm3',
-	'https://drive.google.com/uc?export=view&id=1BMSibckCyFum6w_eFfeiupVP2Up8LZFg',
-	'https://drive.google.com/uc?export=view&id=1Y6YEu4Po8FOJGHO5oz0hA03Vc2d50VNz',
-	'https://drive.google.com/uc?export=view&id=1Y6YEu4Po8FOJGHO5oz0hA03Vc2d50VNz',
-	'https://drive.google.com/uc?export=view&id=1U25zVwaclczh7zgGkSUvUZvWR0iP9zWY',
-	'https://drive.google.com/uc?export=view&id=1lB0JBIvCox7Cvi0VEV2uxePOt02pqVp_',
-	'https://drive.google.com/uc?export=view&id=1C3MC-Tai-W5NNMzmsGfLk92eS4JDpoQe',
-	'https://drive.google.com/uc?export=view&id=1lB0JBIvCox7Cvi0VEV2uxePOt02pqVp_'
+	'https://drive.google.com/uc?export=view&id=102f53qPAvCG5FqPeIjP4TnodWHx89_y4',
+	'https://drive.google.com/uc?export=view&id=12qBEgzveupJMp6KlKaOiNgkPN4QoHcNt',
+	'https://drive.google.com/uc?export=view&id=1QNsK_gt5gcx-iyJQ9K4alkl7h5Dg_fkx',
+	'https://drive.google.com/uc?export=view&id=1a-In48o0nPAGeRZP1U5MXR_cX_QYTgD6',
+	'https://drive.google.com/uc?export=view&id=1lnnnVstpdbfEycHkBzUnQDnn8nAbL9I0',
+	'https://drive.google.com/uc?export=view&id=1pSlv0Bs7Yna_2R2aTooUeXMbBK0xYlw6',
+	'https://drive.google.com/uc?export=view&id=102f53qPAvCG5FqPeIjP4TnodWHx89_y4'
 ];
+
+
